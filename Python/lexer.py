90b4533ed1a016100159913b431d087d0824ac54
bromlu <lukewilliambrom@gmail.com>
2019-03-02 15:06:49 -0500

    Modify lexer to ignore newlines and to return MOD and EOF

--- Python/lexer.py
+++ Python/lexer.py
@@ -2,7 +2,7 @@ class Lexer:
     def __init__(self, inputFile):
         self.file = open(inputFile, 'r')
         self.value = ''
-        self.lineNumber = 0
+        self.lineNumber = 1
         self.characterNumber = 0
 
     def next(self):
@@ -11,19 +11,24 @@ class Lexer:
         self.characterNumber += 1
         
         if token != '':
-            while token == ' ' or token == '\t':
+            while token == ' ' or token == '\t' or token == '\n' or token == '\r':
+                if token == '\n' or token == '\r':
+                    self.characterNumber = 0
+                    self.lineNumber += 1
                 token = self.file.read(1)
                 self.characterNumber += 1
             if token.isalpha():
                 self.value = token
                 token = self.file.read(1)
                 self.characterNumber += 1
-                while token.isalpha():
+                while token.isalpha() or token.isdigit():
                     self.value += token
                     token = self.file.read(1)
                     self.characterNumber += 1
                 self.characterNumber -= 1
                 self.file.seek(self.file.tell() - 1)
+                if self.value == 'MOD' or self.value == 'EOF':
+                    return self.value
                 return 'STRING'
             elif token.isdigit():
                 self.value = token
@@ -36,15 +41,10 @@ class Lexer:
                 self.characterNumber -= 1
                 self.file.seek(self.file.tell() - 1)
                 return 'NUMBER'
-            elif token == '\n' or token == '\r':
-                self.characterNumber = 0
-                self.lineNumber += 1
-                self.value = token
-                return 'NEWLINE'
             elif (token == ';' or token == '(' or token == ')' or token == '+'
                 or token == '-' or token == '*' or token == '/'):
                 self.value = token
-                return 'SYMBOL'
+                return token
             else:
                 return 'ERROR'
         else:
